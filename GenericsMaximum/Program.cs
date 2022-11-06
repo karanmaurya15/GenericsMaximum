ed60d599631042b4292760d05335ac69030c6ebe
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-06 20:49:56 +0530

    Maximum integers is Done

--- GenericsMaximum/Program.cs
+++ GenericsMaximum/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace GenericsMaximum
+﻿using System.Net.Http.Headers;
+
+namespace GenericsMaximum
 {
-    internal class Program
+    public class MaxNumCheck
     {
-        static void Main(string[] args)
+       
+        public static int MaxIntNumber(int firstNumber, int secondNumber, int thirdNumber)
         {
+           
+
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(thirdNumber) > 0 && secondNumber.CompareTo(firstNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdnumber are same");
+           
+        }
+        
+    }
+    
+    internal class program
+    { 
+        static void Main(string[] args) 
+        {
+
             Console.WriteLine("Wellcome to Generics Program");
+            int result = MaxNumCheck.MaxIntNumber(55,43,23);
+            Console.WriteLine(result);
+         
+
         }
     }
 }
