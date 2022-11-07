81dfa7a11ebe75c12f154e489fe62a37e6f2f3da
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-07 21:09:10 +0530

    Take Four Parameter is Done

--- GenericsMaximum/MaximumComputation.cs
+++ GenericsMaximum/MaximumComputation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,40 +8,53 @@ namespace GenericsMaximum
 {
     public class MaximumComputation<K> where K : IComparable
     {
-        public K firstValue, secondvalue, thirdValue;
-        public MaximumComputation(K firstValue, K secondvalue, K thirdValue)
+        public K firstValue, secondvalue, thirdValue, fourthValue;
+        public MaximumComputation(K firstValue, K secondValue, K thirdValue, K fourthValue)
         {
             this.firstValue = firstValue;
-            this.secondvalue = secondvalue;
+            this.secondvalue = secondValue;
             this.thirdValue = thirdValue;
+            this.fourthValue = fourthValue;
         }
 
-        public static K MaxNumber<K>(K firstValue, K secondvalue, K thirdValue) where K : IComparable
+        public static K MaxNumber<K>(K firstValue, K secondValue, K thirdValue, K fourthValue) where K : IComparable
         {
-            if (firstValue.CompareTo(secondvalue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondvalue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondvalue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 && firstValue.CompareTo(fourthValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 && firstValue.CompareTo(fourthValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0 && firstValue.CompareTo(fourthValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 && firstValue.CompareTo(fourthValue) >= 0)
             {
                 return firstValue;
             }
-            if (secondvalue.CompareTo(firstValue) > 0 && secondvalue.CompareTo(thirdValue) > 0 ||
-                secondvalue.CompareTo(firstValue) >= 0 && secondvalue.CompareTo(thirdValue) > 0 ||
-                secondvalue.CompareTo(firstValue) > 0 && secondvalue.CompareTo(thirdValue) >= 0)
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(fourthValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(fourthValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(fourthValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(fourthValue) >= 0)
             {
-                return secondvalue;
+                return secondValue;
             }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondvalue) > 0 ||
-                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondvalue) > 0 ||
-                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondvalue) >= 0)
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(fourthValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(fourthValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(fourthValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(fourthValue) >= 0)
             {
                 return thirdValue;
             }
+            if (fourthValue.CompareTo(firstValue) > 0 && fourthValue.CompareTo(secondValue) > 0 && fourthValue.CompareTo(thirdValue) > 0 ||
+                fourthValue.CompareTo(firstValue) >= 0 && fourthValue.CompareTo(secondValue) > 0 && fourthValue.CompareTo(thirdValue) > 0 ||
+                fourthValue.CompareTo(firstValue) > 0 && fourthValue.CompareTo(secondValue) >= 0 && fourthValue.CompareTo(thirdValue) > 0 ||
+                fourthValue.CompareTo(firstValue) > 0 && fourthValue.CompareTo(secondValue) > 0 && fourthValue.CompareTo(thirdValue) >= 0)
+            {
+                return fourthValue;
+            }
+
             return firstValue;
         }
         public K MaxMethod()
         {
-            K Max = MaximumComputation<K>.MaxNumber(this.firstValue, this.secondvalue, this.thirdValue);
+            K Max = MaximumComputation<K>.MaxNumber(this.firstValue, this.secondvalue, this.thirdValue, this.fourthValue);
             return Max;
+
         }
     }
 }
--- GenericsMaximum/Program.cs
+++ GenericsMaximum/Program.cs
@@ -7,13 +7,14 @@ namespace GenericsMaximum
     { 
         static void Main(string[] args) 
         {
-           
+
+
             Console.WriteLine("Wellcome to Generics Program\n");
-            MaximumComputation<int> maximumInt = new MaximumComputation<int>(56, 245, 45);
+            MaximumComputation<int> maximumInt = new MaximumComputation<int>(56, 245, 45, 32);
             Console.WriteLine("Maximum Number is : " + maximumInt.MaxMethod());
-            MaximumComputation<float> maximumFloat = new MaximumComputation<float>(17.9f, 20.6f, 309.5f);
+            MaximumComputation<float> maximumFloat = new MaximumComputation<float>(17.9f, 20.6f, 309.5f, 4.7f);
             Console.WriteLine("Maximum Number is : " + maximumFloat.MaxMethod());
-            MaximumComputation<string> maximumString = new MaximumComputation<string>("Apple", "AA", "Data");
+            MaximumComputation<string> maximumString = new MaximumComputation<string>("Apple", "AA", "Data", "Input");
             Console.WriteLine("Maximum Number is : " + maximumString.MaxMethod());
 
 
